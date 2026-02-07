64b400c33b52962fa32ec91da92a825398f69803
Killian <killian2100@hotmail.com>
2026-02-07 17:54:02 +0000

    Configured settings for GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,21 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '' : ''
+		}
+	}
+};
 
 export default config;
